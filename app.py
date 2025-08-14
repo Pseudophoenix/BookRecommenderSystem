9a078f0c3141bd0388043f21e6081248eb06cdef
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-14 12:03:40 +0530

    Initial commit

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import pickle
 import numpy as np
 
 
-# popular_df=pickle.load(open('popular.pkl','rb'))
-# pt=pickle.load(open('pt.pkl','rb'))
-# books=pickle.load(open('books.pkl','rb'))
-# similarity_scores=pickle.load(open('similarity_score.pkl','rb'))
+popular_df=pickle.load(open('popularity.pkl','rb'))
+pt=pickle.load(open('pt.pkl','rb'))
+books=pickle.load(open('books.pkl','rb'))
+similarity_scores=pickle.load(open('similarity_scores.pkl','rb'))
 app=Flask(__name__)
 
 
@@ -16,8 +16,8 @@ def index():
                            book_name=list(popular_df['Book-Title'].values),
                            author=list(popular_df['Book-Author'].values),
                            image=list(popular_df['Image-URL-M'].values),
-                           votes=list(popular_df['num_ratings'].values),
-                           rating=list(popular_df['avg_rating'].values)
+                           votes=list(popular_df['num_rating'].values),
+                           rating=list(popular_df['avg_rating'].values),
                            )
 
 @app.route('/recommend')
@@ -35,7 +35,7 @@ def recommend():
         temp_df=books[books['Book-Title']==pt.index[i[0]]]
         item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
         item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-URL-M'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
         data.append(item)
     print(data)
     return render_template('recommend.html',data=data)
